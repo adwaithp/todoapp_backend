438eb1c0c7d1ed74434b683b9e56f45ec61bf02f
Adwaith P <adwaithp@vofox.local>
2023-10-28 15:41:22 +0530

    initial commit

---
+++ todoapp/serializers.py
@@ -0,0 +1,8 @@
+# serializers.py
+from rest_framework import serializers
+from .models import TodoItem
+
+class TodoItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoItem
+        fields = '__all__'
---
+++ todoapp/urls.py
@@ -0,0 +1,8 @@
+# urls.py
+from django.urls import path
+from .views import TodoItemList, TodoItemDetail
+
+urlpatterns = [
+    path('todos/', TodoItemList.as_view(), name='todo-list'),
+    path('todos/<int:pk>/', TodoItemDetail.as_view(), name='todo-detail'),
+]
